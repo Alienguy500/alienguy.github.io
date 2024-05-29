9adfd0ccf61d3c25c88f42b52e57b226dc3fe622
Alienguy500 <alienguy500@gmail.com>
2024-05-29 23:47:30 +0100

    Moved all scripts to sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -1,11 +1,15 @@
-/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+// open the sidebar
 function openNav() {
     document.getElementById("mySidebar").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("hamburger").textContent="×";
+    document.getElementById("hamburger").onclick = closeNav;
 }
-  
-/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-function closeNav() {
+
+// close the sidebar
+  function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.getElementById("hamburger").textContent="☰";
+    document.getElementById("hamburger").onclick = openNav;
 }
---
+++ typingText.js
@@ -0,0 +1,5 @@
+// Typing animation for text elements
+var index = 0;
+function typeThisText(text,currentIndex) {
+    this.textContent="";
+}
