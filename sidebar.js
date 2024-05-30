d7334bb260ad44409fdf20c12c92ea72cd15bbff
Alienguy500 <alienguy500@gmail.com>
2024-05-30 16:14:58 +0100

    Added typing animation

--- sidebar.js
+++ sidebar.js
@@ -2,6 +2,7 @@
 function openNav() {
     document.getElementById("mySidebar").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("mainContent").style.marginLeft = "270px";
     document.getElementById("hamburger").textContent="×";
     document.getElementById("hamburger").onclick = closeNav;
 }
@@ -10,6 +11,26 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
+    document.getElementById("mainContent").style.marginLeft = "20px";
     document.getElementById("hamburger").textContent="☰";
     document.getElementById("hamburger").onclick = openNav;
 }
+
+var buttonHoverSound = new Audio("Button Pressed.wav");
+var buttonUnhoverSound = new Audio("Button Unpressed.wav");
+
+const hamburgerbtn = document.getElementById("hamburger");
+
+hamburgerbtn.addEventListener(
+  "mouseover",
+  (event) => {
+    buttonHoverSound.play();
+  }
+)
+
+hamburgerbtn.addEventListener(
+  "mouseleave",
+  (event) => {
+    buttonUnhoverSound.play();
+  }
+)
--- typingText.js
+++ typingText.js
@@ -1,5 +1,15 @@
 // Typing animation for text elements
-var index = 0;
-function typeThisText(text,currentIndex) {
-    this.textContent="";
+
+function startTyping(elementId) {
+    var textToGet = document.getElementById(elementId);
+    var index = 1;
+    const originalText = textToGet.textContent;
+    typeThisText(textToGet,index,originalText);
+}
+
+function typeThisText(element,currentIndex,originalText) {
+    element.textContent = originalText.slice(0,currentIndex) + "_";
+    if (currentIndex < originalText.length-1) {
+        setTimeout(function() {typeThisText(element,currentIndex+1,originalText)}, 100);
+    }
 }
